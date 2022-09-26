ef03bf5f0c03b9381729ba6dc42689b882ca02d8
oyvindlr <oyvindlr@gmail.com>
2022-09-26 13:54:16 +0200

    Fixed bug from previous commit

--- ParForWaitbarCreateMH.m
+++ ParForWaitbarCreateMH.m
@@ -6,15 +6,15 @@ hWaitbar = waitbar(0,Msg, 'windowstyle', 'modal');
 % Initialize the Fractional Length of the Waitbar 
 set(hWaitbar,'UserData',0)
 
-try
-    ver('parallel');%Fails if parallel computing toolbox is not installed
+info = ver('parallel');%Empty if parallel computing toolbox is not installed
 
+if ~isempty(info)
     % Calling the constructor  for a DataQueue to enable sending data from
     % workers back to the client in a parallel pool
     hWaitbarMsgQueue = parallel.pool.DataQueue;
 
     % Define a function to call when new data is received
     hWaitbarMsgQueue.afterEach(@(x)ParForWaitbarProgressMH(hWaitbar,NbrePts,Msg));
-catch
+else
     hWaitbarMsgQueue = NoParallelUpdater(hWaitbar, NbrePts, Msg);
 end
